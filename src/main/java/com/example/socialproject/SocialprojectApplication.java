abd9b845e5462e1201cb7dea689f1cc02bd80f25
lewis <htdke12@gmail.com>
2019-02-23 16:24:55 -0500

    setup springboot project

---
+++ src/main/java/com/example/socialproject/SocialprojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.socialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialprojectApplication {
+
+	public static void main(String[] args) {
+		//hjghjgjhg
+		SpringApplication.run(SocialprojectApplication.class, args);
+	}
+
+}
